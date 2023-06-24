218530346b2fc3284dc8dd88129427e4ed7a6bdc
YoussefHissi <youssefhissi789@gmail.com>
2023-06-24 14:56:06 +0200

    import packages

--- app/Models/UserPlan.ts
+++ app/Models/UserPlan.ts
@@ -5,14 +5,14 @@ export default class UserPlan extends BaseModel {
   @column({ isPrimary: true })
   public id: number
 
-  @column.dateTime({ autoCreate: true })
-  public createdAt: DateTime
-
   @column()
   public Date_Payment: DateTime
 
   @column()
   public Value: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
